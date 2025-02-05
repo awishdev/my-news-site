34b4e4858b2c5995568e71b2abae8a2549455f11
Arthur Ish <arthurishdev@gmail.com>
2025-02-05 00:38:28 -0800

    fixed css styling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useState, useEffect} from "react";
 import { Outlet } from "react-router-dom";
-//import './App.css';
+import './App.css';
 import NavBar from './components/NavBar';
 
 function App() {
